4a295c26cdc021258de112cffd1fa76bb581f810
Evangelion Manuhutu <evangelion11.m@gmail.com>
2024-06-22 02:07:04 +0700

    definined IMGUI_DEFINE_MATH_OPERATORS

--- GraphEditor.cpp
+++ GraphEditor.cpp
@@ -24,9 +24,12 @@
 // SOFTWARE.
 //
 
-#include "imgui.h"
+#ifndef IMGUI_DEFINE_MATH_OPERATORS
 #define IMGUI_DEFINE_MATH_OPERATORS
-#include "imgui_internal.h"
+#endif
+
+#include <imgui.h>
+#include <imgui_internal.h>
 #include <math.h>
 #include <vector>
 #include <float.h>
